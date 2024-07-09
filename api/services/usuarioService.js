ef13309b3dffe1bb36e769dd1b733c205057aa67
Dirceusljr <dirceusljr@gmail.com>
2024-07-09 16:26:14 -0300

    instalação do bcryptjs + hash da senha + implementação do servico de cadastrar usuário + cadastro de usuário

--- api/services/usuarioService.js
+++ api/services/usuarioService.js
@@ -1,4 +1,6 @@
 const database = require('../models')
+const { hash } = require('bcryptjs');
+const uuid = require('uuid');
 
 class UsuarioService {
     async cadastrar(dto) {
@@ -8,11 +10,24 @@ class UsuarioService {
             }
         })
     
-        if (usuario) {
-            throw new Error('Usuário já cadastrado!')
+        try {
+            if (usuario) {
+                throw new Error('Usuário já cadastrado!')
+            }
+    
+            const senhaHash = await hash(dto.senha, 8)
+    
+            const novoUsuario = database.usuarios.create({
+                id: uuid.v4(),
+                nome: dto.nome,
+                email: dto.email,
+                senha: senhaHash
+            })
+    
+            return novoUsuario
+        } catch (error) {
+            throw new Error('Erro ao cadastrar usuário.')
         }
-
-        database.usuarios.insert(dto)
     }
 }
 
