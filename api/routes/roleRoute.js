a84a02fa7eef1645c91e78bb26cf98a21b89faa8
Dirceusljr <dirceusljr@gmail.com>
2024-07-10 15:25:05 -0300

    criando a model roles + criando a rota role + iniciando o crud de role

---
+++ api/routes/roleRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+
+
+const router = Router()
+
+router
+    .post('/role')
+    .get('/role')
+    .get('/role/:id')
+    .put('/role/:id')
+    .delete('/role/:id')
+
+module.exports = router
---
+++ api/services/roleService.js
@@ -0,0 +1,33 @@
+const database = require('../models')
+const uuid = require('uuid')
+
+class RoleService {
+    async cadastrar(dto) {
+        const role = database.roles.findOne({
+            where: {
+                nome: dto.nome
+            }
+        })
+
+        if(role) {
+            throw new Error('Role já cadastrada.')
+        }
+
+        try {
+            const newRole = database.roles.create({
+                id: uuid.v4(),
+                nome: dto.nome,
+                descricao: dto.descricao
+            })
+
+
+            
+        } catch (error) {
+            throw new Error('Erro ao cadastrar role.')
+        }
+
+    }
+
+}
+
+module.exports = RoleService
