8f312c5627c5984be29efabfdb0749e5bb5979d0
Dirceusljr <dirceusljr@gmail.com>
2024-07-11 16:19:49 -0300

    implementando o service seguranca de cadastrarAcl

--- api/services/segurancaService.js
+++ api/services/segurancaService.js
@@ -1,16 +1,17 @@
+const { Sequelize } = require("sequelize");
 const database = require("../models");
 
 class SegurancaService {
   async cadastrarAcl(dto) {
-    const usuario = database.usuarios.findOne({
-      indcludes: [
+    const usuario = await database.usuarios.findOne({
+      include: [
         {
-          model: "database.roles",
+          model: database.roles,
           as: "usuario_roles",
           attributes: ["id", "nome", "descricao"],
         },
         {
-            model: "database.permissoes",
+            model: database.permissoes,
             as: "usuario_permissoes",
             attributes: [ "id", "nome", "descricao" ]
         },
@@ -19,7 +20,54 @@ class SegurancaService {
         id: dto.usuarioId,
       },
     });
-  }
+
+    if(!usuario) {
+        throw new Error('Usuário não cadastrado.')
+    }
+
+    const rolesCadastradas = await database.roles.findAll({
+        where: {
+            id: {
+                [Sequelize.Op.in]: dto.roles
+            }
+        }
+    })
+
+    const permissoesCadastradas = await database.permissoes.findAll({
+        where: {
+            id: {
+                [Sequelize.Op.in]: dto.permissoes
+            }
+        }
+    })
+
+    await usuario.removeUsuario_roles(usuario.usuario_roles)
+    await usuario.removeUsuario_permissoes(usuario.usuario_permissoes)
+
+    await usuario.addUsuario_roles(rolesCadastradas)
+    await usuario.addUsuario_permissoes(permissoesCadastradas)
+
+    const novoUsuario = await database.usuarios.findOne({
+        include: [
+          {
+            model: database.roles,
+            as: "usuario_roles",
+            attributes: ["id", "nome", "descricao"],
+          },
+          {
+              model: database.permissoes,
+              as: "usuario_permissoes",
+              attributes: [ "id", "nome", "descricao" ]
+          },
+        ],
+        where: {
+            id: dto.usuarioId,
+          },
+      });
+
+      return novoUsuario
+
+}
 }
 
 module.exports = SegurancaService;
