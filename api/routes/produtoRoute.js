b95412d6dc8823739288fa364711032811ccf75f
Dirceusljr <dirceusljr@gmail.com>
2024-07-12 14:12:25 -0300

    middleware de permissao por perfil de usuario

--- api/routes/produtoRoute.js
+++ api/routes/produtoRoute.js
@@ -2,13 +2,14 @@ const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController')
 const roles = require('../middleware/roles')
 const permissoes = require('../middleware/permissoes')
+const permissoesRoles = require('../middleware/permissoesRoles')
 
 
 const router = Router()
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', permissoes("editar"), ProdutoController.buscarTodosProdutos)
+  .get('/produto', permissoesRoles("editar"), ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
   .put('/produto/id/:id', ProdutoController.editarProduto)
